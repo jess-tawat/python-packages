6dd265c3b10033496887223c6fc3f40fde7783be
Jess Tawat <jesstawat@gmail.com>
2023-07-23 18:33:38 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "nconnect-python-package",
+    version = "0.0.1",
+    author = "Jess Tawat",
+    author_email = "jess.tawat@gmail.com",
+    description = ("nConnect Python Package"),
+    url = "https://github.com/jess-tawat/python-packages.git",
+    packages=[
+        'hello'
+    ],
+)
